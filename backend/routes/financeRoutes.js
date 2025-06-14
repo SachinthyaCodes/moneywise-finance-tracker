c534d079f90cd9ad22239fe1840f76206fec3429
Gimhani <127582749+Gimhani09@users.noreply.github.com>
2025-06-15 01:20:48 +0530

    Delete backend directory

--- backend/routes/financeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getFinancialSuggestions } = require("../controllers/financeController");
-
-const router = express.Router();
-
-router.post("/get-suggestions", getFinancialSuggestions);
-
-module.exports = router;
